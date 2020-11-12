bac94adec72001a9ee414416770caf350d87c9df
Pavel Zhelnov <pzhelnov@p1m.org>
2020-11-12 03:31:26 +0300

    Upload Nov 10 search&editables (n=490)
    Ref. https://github.com/drzhelnov/zheln.github.io/issues/100

--- zheln/general-makeposti.sh
+++ zheln/general-makeposti.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 v='3.1.1'
 edit=true
-date='2020-11-09'
-count=525
+date='2020-11-10'
+count=490
 
-coreutils=true
+coreutils=false
 rm_record_set=true
 
 summary_set='summary-systematic-set'
