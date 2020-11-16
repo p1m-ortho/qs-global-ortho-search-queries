b24f84872f2d92aaba7e02948b5508663425d85f
Pavel Zhelnov <pzhelnov@p1m.org>
2020-11-16 06:48:26 +0300

    Upload Nov 13 search&editables (n=558)
    Worked time low because forgot to turn on
    stopwatch when started, just noticed later.
    
    Ref. https://github.com/drzhelnov/zheln.github.io/issues/107

--- zheln/general-makeposti.sh
+++ zheln/general-makeposti.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 v='3.1.1'
 edit=true
-date='2020-11-12'
-count=759
+date='2020-11-13'
+count=558
 
-coreutils=true
+coreutils=false
 rm_record_set=true
 
 summary_set='summary-systematic-set'
