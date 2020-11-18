083587ac2b75f47875f0792ad5f8e80b55d22c29
Pavel Zhelnov <pzhelnov@p1m.org>
2020-11-18 13:30:41 +0300

    Make Page 1.1.0: pages dir & prep for day pages
    Moved so that these files didn’t flood root
    repo page on GitHub and so that all generated
    by Make Page was in one place.
    
    Ref. regarding day pages: https://github.com/drzhelnov/zheln.github.io/issues/101

--- zheln/make-page.sh
+++ zheln/make-page.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-v='1.0.0'
+v='1.1.0'
 coreutils=true
 files='zheln_ama_specialty_page_filenames.lst'
 html='unspecified.html'
@@ -9,6 +9,8 @@ titles='zheln_ama_specialty_titles.lst'
 unspecified_tag='unspecified'
 unspecified_code='US'
 unspecified_title='Unspecified'
+pages='pages'
+specialty='specialty'
 echo "> I am Make Page v$v.
 > ’kay, want coreutils?
 $coreutils
@@ -29,29 +31,35 @@ if
 else echo '> Text files in place.'
 fi
 
-if [ -d "pages" ]; then
+if [ -d "$pages" ]; then
   echo '> Leftovers present. Check with them first.'
   exit 1
 else echo '> No leftovers detected.'; echo '> Proceed to execution…'
 fi
 
-mkdir pages; cd pages
+mkdir "$pages"; cd "$pages"
 i=0; k=0
+mkdir "$specialty"; cd "$specialty"
 while read file; do
-  cp "../$html" "$file"
+  cp "../../$html" "$file"
   content=$(< $file); while read title; do
     if [ "$k" = "$i" ]; then echo "${content/title: $unspecified_title/title: $title}" > "$file"; fi
     k=$((k + 1))
-  done < "../$titles"; k=0
+  done < "../../$titles"; k=0
   content=$(< $file); while read tag; do
     if [ "$k" = "$i" ]; then echo "${content/$unspecified_tag/$tag}" > "$file"; fi
     k=$((k + 1))
-  done < "../$tags"; k=0
+  done < "../../$tags"; k=0
+  content=$(< $file); while read tag; do
+    tag="${tag// /-}"
+    if [ "$k" = "$i" ]; then echo "${content/permalink: \/$unspecified_tag\//permalink: /$tag/}" > "$file"; fi
+    k=$((k + 1))
+  done < "../../$tags"; k=0
   content=$(< $file); while read code; do
     if [ "$k" = "$i" ]; then echo "${content/site.$unspecified_code/site.$code}" > "$file"; fi
     k=$((k + 1))
-  done < "../$codes"; k=0
+  done < "../../$codes"; k=0
   i=$((i + 1))
-done < "../$files"
+done < "../../$files"
 
 echo '> Mission accomplished.'; exit 0
