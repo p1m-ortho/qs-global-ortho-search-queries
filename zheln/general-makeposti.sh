d490ac4b43dd92b523da42398c048357e63ae5e1
Pavel Zhelnov <pzhelnov@p1m.org>
2020-12-26 07:41:39 +0300

    Start Step 6 for Dec r51d17
    Ref. https://github.com/drzhelnov/zheln.github.io/issues/189

--- zheln/general-makeposti.sh
+++ zheln/general-makeposti.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 v='3.1.1'
-edit=true
-date='2020-12-21'
-count=696
+edit=false
+date='2020-12-17'
+count=704
 
-coreutils=true
+coreutils=false
 rm_record_set=true
 
 summary_set='summary-systematic-set'
